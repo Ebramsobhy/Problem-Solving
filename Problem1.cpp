ec891702bafa5f1e2e63f7f2877e9ae227881e63
Ebramsobhy <abramsobhy7@gmail.com>
2024-03-03 23:40:37 +0200

    Done ❤️

---
+++ Problem1.cpp
@@ -0,0 +1,21 @@
+/*
+   =============================================
+   Problem 1
+   Write a program to print your name on screen
+   =============================================
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintName(string name)
+{
+  cout << "\n Your name is: " << name << endl;
+}
+
+int main()
+{
+  PrintName("Ebram Sobhy");
+  return 0;
+}
