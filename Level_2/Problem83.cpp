221e1cbf730f12461fbc394f356cd04cdb84aa66
Ebramsobhy <abramsobhy7@gmail.com>
2024-04-05 09:45:44 +0200

    Done ❤️

--- Level_2/Problem83.cpp
+++ Level_2/Problem83.cpp
@@ -1,12 +1,12 @@
 /*
    ====================================================================================
-    Problem 82
+    Problem 83
     Write a program to fill array with max size 100 with random numbers from 1 to 100
-    then copy it to another array using AddArrayElement and print it.
-    Input: 10
-    Output: 
-        Array 1 Elements: 42 68 35 1 70 25 79 59 63 65 
-        Array 2 Elements: 42 68 35 1 70 25 79 59 63 65       
+    then copy only odd numbers to another array using AddArrayElement and print it.
+    Input: 5
+    Output:
+       Array 1 Elements:    5 10 31 44 21
+       Array 2 Odd Numbers: 5 31 21
    ====================================================================================
 */
 
@@ -22,7 +22,6 @@ int RandomNumber(int From, int To)
     int randNum = rand() % (To - From + 1) + From; 
     return randNum; 
 } 
-
 
 void FilledArrayWithRandomNumbers(int arr[100], int& arrLength)
 {
@@ -41,11 +40,14 @@ void AddArrayElement(int Number, int arr[100], int& arrLength)
    arrLength++;
 }
 
-void CopyArrayUsingAddArrayElement(int arrSource[100], int arrDestination[100], int arrLength, int& arr2Length)
+void CopyOddNumbersInNewArray(int arrSource[100], int arrDestination[100], int arrLength, int& arr2Length)
 {
     for (int i = 0; i < arrLength; i++)
     {
-       AddArrayElement(arrSource[i], arrDestination, arr2Length);
+       if (arrSource[i] % 2 != 0)
+       {
+            AddArrayElement(arrSource[i], arrDestination, arr2Length);
+       }
     } 
 }
 
@@ -60,18 +62,18 @@ void PrintArray(int arr[100], int arrLength)
 
 int main()
 {
-   srand((unsigned)time(NULL)); 
-   
+   srand((unsigned)time(NULL));
+
    int arr[100], arrLength = 0, arr2Length = 0;
    FilledArrayWithRandomNumbers(arr, arrLength);
    
    int arr2[100];
-   CopyArrayUsingAddArrayElement(arr, arr2, arrLength, arr2Length);
+   CopyOddNumbersInNewArray(arr, arr2, arrLength, arr2Length);
 
    cout << "Array 1 Elements:\n"; 
    PrintArray(arr, arrLength); 
    
-   cout << "Array 2 Elements After Copy:\n"; 
+   cout << "Array 2 Odd Numbers\n"; 
    PrintArray(arr2, arr2Length); 
    return 0;
 }
